6f4000fb6634d2290c071168ae8e85a3447f54e6
David Laprade <david.laprade@gmail.com>
2022-09-22 16:28:56 -0400

    Enable partial withdrawals based on user-input amounts

--- src/components/AavePartialWithdraw.tsx
+++ src/components/AavePartialWithdraw.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import config from '../contractConfig.json'
+import aTokenConfig from '../contractConfig.json'
 import {
+  useAccount,
   useSendTransaction,
   usePrepareSendTransaction,
-  useContractRead,
+  useContractReads,
   usePrepareContractWrite,
   useContractWrite
 } from 'wagmi'
@@ -11,24 +12,37 @@ import { UseContractConfig } from 'wagmi/dist/declarations/src/hooks/contracts/u
 import { BigNumber, constants } from 'ethers'
 import { parseEther } from 'ethers/lib/utils'
 
-// TODO disable if wallet is not connected
-
-const contractConfig: UseContractConfig = {
-  addressOrName: config.address,
-  contractInterface: config.abi
+const aTokenContract: UseContractConfig = {
+  addressOrName: aTokenConfig.address,
+  contractInterface: aTokenConfig.abi
 }
 
 interface Props {
-  withdrawContractAddr: string
+  withdrawContractAddr: string;
+  aTokenBalance: BigNumber;
+  aTokenWithdrawRouterAllowance: BigNumber;
 }
 
-export default function AavePartialWithdraw({withdrawContractAddr} : Props) {
-  const [amount, setAmount] = useState('0')
+export default function AavePartialWithdraw(
+  {
+    withdrawContractAddr,
+    aTokenBalance,
+    aTokenWithdrawRouterAllowance,
+  } : Props
+) {
+  const [amount, setAmount] = useState<string | undefined>();
+  const [readyForWithdraw, setReadyForWithdraw] = useState<boolean>(false);
+  const [withdrawClicked, setWithdrawClicked] = useState<boolean>(false);
+
+  const parsedAmount = (): BigNumber => {
+    if (amount) return parseEther(parseFloat(amount).toString());
+    return parseEther("0");
+  }
 
   const { config: approveATokenConfig } = usePrepareContractWrite({
-    ...contractConfig,
+    ...aTokenContract,
     functionName: 'approve',
-    args: [config.address, parseEther(amount)]
+    args: [withdrawContractAddr, parsedAmount()]
   })
 
   const {
@@ -38,27 +52,37 @@ export default function AavePartialWithdraw({withdrawContractAddr} : Props) {
     write: approveATokenSpend,
   } = useContractWrite(approveATokenConfig)
 
+  const approveATokenSpendIfNecessary = () => {
+    setWithdrawClicked(true);
+
+    // We don't need to approve if there is already sufficient allowance.
+    if (aTokenWithdrawRouterAllowance.lt(parsedAmount())) {
+      approveATokenSpend?.();
+    } else {
+      setReadyForWithdraw(true);
+    }
+  }
+
+  const dataForWithdrawTx = ():string => {
+    const percent = parsedAmount().mul("100").div(aTokenBalance);
+    // We multiply by 255 because that is the maximum value that can be stored
+    // in 2 bytes. So we express the percentage in terms of 255. In a better
+    // version of this algorithm, we would dynamically determine the data length
+    // in bytes necessary to acheive a certain level of fidelity with the
+    // user-specified amount. For this demo, being able to specify amounts only
+    // in 0.4 percentage increments (100/255) is good enough.
+    return percent.mul("255").div("100").toHexString();
+  }
+
   // Withdraw token
   const { config: withdrawConfig } = usePrepareSendTransaction({
     request: {
       to: withdrawContractAddr,
       value: 0,
+      data: dataForWithdrawTx(),
       gasLimit: 1e6,
     },
   })
-  // maybe need to do something like:
-  // const {
-  //   config,
-  //   error: prepareError,
-  //   isError: isPrepareError,
-  // } = usePrepareContractWrite({
-  //   addressOrName: config.address,
-  //   contractInterface: [],
-  //   functionName: "fallback",
-  //   args: ["0x42"],
-  // });
-  // const { data, error, isError, write
-  // } = useContractWrite(config);
 
   const {
     data: withdrawData,
@@ -68,28 +92,43 @@ export default function AavePartialWithdraw({withdrawContractAddr} : Props) {
   } = useSendTransaction(withdrawConfig);
 
   useEffect(() => {
-    if(isApproveTokenSuccess && sendWithdrawTransaction) {
-      sendWithdrawTransaction?.()
-    }
-  },[isApproveTokenSuccess, sendWithdrawTransaction])
+    if(isApproveTokenSuccess) setReadyForWithdraw(true)
+  }, [isApproveTokenSuccess])
 
+  useEffect(() => {
+    if (readyForWithdraw && withdrawClicked) {
+      sendWithdrawTransaction?.();
+      setReadyForWithdraw(false);
+      setWithdrawClicked(true);
+    }
+  }, [readyForWithdraw, withdrawClicked])
+
+  if (isWithdrawSuccess) {
+    return(
+      <div>
+        Withdrawal Succeeded! View on <a href={`https://optimistic.etherscan.io/tx/${withdrawData?.hash}`}>Etherscan</a>
+      </div>
+    );
+  }
 
   return (
     <form
-      className="outline outline-black outline-solid rounded-md"
-      onSubmit={() => approveATokenSpend?.()}
+      className="outline outline-black outline-solid rounded-md w-1/2"
+      onSubmit={(event) => {
+        event.preventDefault();
+        approveATokenSpendIfNecessary();
+      }}
     >
       <input
+        className="p-4"
         aria-label="Amount (AToken)"
         onChange={(e) => setAmount(e.target.value)}
         placeholder="0.05"
         value={amount}
       />
-      <button className="p-4 mt-2 bg-green-200 rounded-md hover:bg-green-300">
+      <button className="p-4 m-2 bg-green-200 rounded-md hover:bg-green-300">
         Partial Withdraw from Aave
       </button>
-      {isWithdrawLoading && <div>Check Wallet</div>}
-      {isWithdrawSuccess && <div>Withdrawal Succeeded! {JSON.stringify(withdrawData)}</div>}
     </form>
   )
 }
