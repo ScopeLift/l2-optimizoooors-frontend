889bd58c9c7da3cbbe90d4a3a35080f95d3c0504
David Laprade <david.laprade@gmail.com>
2022-09-23 14:05:33 -0400

    Use the tailwind-input class

--- src/components/AaveDeposit.tsx
+++ src/components/AaveDeposit.tsx
@@ -52,12 +52,7 @@ export default function AaveDeposit({ to }: Props) {
             ref={depositInput}
             placeholder="0.0"
             value={amount}
-            className="box-content py-2 w-32 text-center h-5 mr-2 outline-none bg-transparent ring-none
-            rounded-xl [appearance:textfield]
-            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-            invalid:border-red-500 invalid:text-red-600
-            focus:invalid:border-red-500 focus:invalid:ring-red-500
-          "
+            className="tailwind-input"
           />
         </div>
 
--- src/components/AaveWithdraw.tsx
+++ src/components/AaveWithdraw.tsx
@@ -130,11 +130,7 @@ export default function AavePartialWithdraw(
       <input
         type="number"
         step="0.000001"
-        className="box-content py-2 w-32 text-left h-5 mr-2 outline-none bg-transparent ring-none
-        rounded-xl [appearance:textfield]
-        disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-        invalid:border-red-500 invalid:text-red-600
-        focus:invalid:border-red-500 focus:invalid:ring-red-500"
+        className="tailwind-input"
         aria-label="Amount (AToken)"
         onChange={(e) => setAmount(e.target.value)}
         placeholder="0.0"
